982404ad5309ff384b7ca6da56c1f0022c225f4a
mohamim360 <mohamim360@gmail.com>
2023-06-16 03:29:30 +0600

    jwt token

--- index.js
+++ index.js
@@ -31,6 +31,12 @@ async function run() {
     const selectedCollection = client.db("learning-school").collection("selects");
     const usersCollection = client.db("learning-school").collection("users");
 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+      res.send({ token })
+    })
 
     app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray();
